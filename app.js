aa40000debcda6eeab76e831955854f8dc0d483f
felipejsm <felipejsm41@gmail.com>
2017-09-16 08:15:49 -0300

    Removing inner code from controller

--- app.js
+++ app.js
@@ -14,8 +14,6 @@
    angular
         .module('app',['firebase'])
         .controller('myCtrl', function($firebaseObject) {
-            const rootRef = firebase.database().ref().child('angular');
-            const ref = rootRef.child('object');
-            this.object = $firebaseObject(ref);
+            
         })
 }());
