678fb6345a921e473e15549e207fab18aebd8d5e
felipejsm <felipejsm41@gmail.com>
2017-09-19 07:09:33 -0300

    Logic added

--- app.js
+++ app.js
@@ -15,5 +15,30 @@
         .module('app',['firebase'])
         .controller('myCtrl', function($firebaseObject, $scope) {
           $scope.thirst = false;
+          $scope.water = 0;
+          $scope.phrase = '';
+          $scope.myColorStyle = {"color":""};
+          $scope.addWater = function(val) {
+              $scope.water += val;
+              if($scope.water >= 0 && $scope.water <= 300) {
+                $scope.phrase = "Bad news, you're going to die of dehydration :(";
+                $scope.myColorStyle = {"color":"#FF4136"};
+              } else if($scope.water > 300 && $scope.water <= 600) {
+                $scope.phrase = "That's all you got?";
+                $scope.myColorStyle = {"color":"#85144b"};
+              } else if($scope.water > 600 && $scope.water <= 1000) {
+                $scope.phrase = "uhm, you're warming up";
+                $scope.myColorStyle = {"color":"#F012BE"};
+              } else if($scope.water > 1000 && $scope.water <= 1500) {
+                $scope.phrase = "keep it up!";
+                $scope.myColorStyle = {"color":"#39CCCC"};
+              } else if($scope.water > 2000) {
+                $scope.myColorStyle = {"color":"#0074D9"};
+                $scope.phrase = "wooow, jeez! you're so hydrated ;)";
+              } else if($scope.water > 3000) {
+                $scope.myColorStyle = {"color":"#FF4136"};
+                $scope.phrase = "Bad news, you're going to die of overhydration :'(";
+              }
+          }
         });
 }());
