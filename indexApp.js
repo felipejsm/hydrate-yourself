54adab8575c503b901ddc84c2114088f674db51a
felipejsm <felipejsm41@gmail.com>
2017-09-24 11:52:34 -0300

    login app js

---
+++ indexApp.js
@@ -0,0 +1,23 @@
+(function() {
+    // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAfcQ_zUfTElCXF196cOHT7wYDVKdDUJRQ",
+    authDomain: "hydrate-yourself.firebaseapp.com",
+    databaseURL: "https://hydrate-yourself.firebaseio.com",
+    projectId: "hydrate-yourself",
+    storageBucket: "hydrate-yourself.appspot.com",
+    messagingSenderId: "206134774012"
+  };
+  firebase.initializeApp(config);
+   
+   angular
+        .module('app',['firebase'])
+        .controller('myCtrl', function($firebaseObject, $scope) {
+            $scope.createUser = createUser;
+            $scope.email;
+            $scope.password;
+            function createUser() {
+  
+            }
+    });
+}());
